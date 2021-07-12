acc77f3e93019aad9baa2771eb9dd0965ab96340
DariaShantalova <darya.shantalova@phystech.edu>
2021-07-12 14:55:34 +0300

    initial commit

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import re
 
 def get_ip_from_cfg(file):
     result = {}
-    elements = open(file)
-    for el in elements:
-        if el.startswith('interface'):
-            interface = (el.split(' ')[1]).split('\n')[0]
+    with open(file) as src:
+        for el in src:
+            if el.startswith('interface'):
+                interface = (el.split(' ')[1]).split('\n')[0]
 
-        match = re.search(r'ip address (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[^0-9])(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[^0-9])', el)
+            match = re.search(r'ip address (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[^0-9])(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}[^0-9])', el)
 
-        if match:
-            ipaddress = match.group(1).split(' ')[0]
-            mask = match.group(2).split('\n')[0]
-            if interface in result.keys():
-                tmp = list()
-                tmp.append(result[interface])
-                tmp.append((ipaddress, mask))
-                result[interface] = tmp
-            else:
-                result[interface] = (ipaddress, mask)
+            if match:
+                ipaddress = match.group(1).split(' ')[0]
+                mask = match.group(2).split('\n')[0]
+                if interface in result.keys():
+                    tmp = list()
+                    tmp.append(result[interface])
+                    tmp.append((ipaddress, mask))
+                    result[interface] = tmp
+                else:
+                    result[interface] = (ipaddress, mask)
     return result
 
 
